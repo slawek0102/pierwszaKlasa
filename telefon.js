7d7c1293948352847182a781b845615c47b58708
Slawomir <grzebyk.slawomir@gmail.com>
2016-07-12 11:59:56 +0200

    Programowanie Objektowe w JS

--- telefon.js
+++ telefon.js
@@ -5,13 +5,16 @@ var Telefon = (function () {
         this.color = color;
     }
     Telefon.prototype.printInfo = function () {
-        console.log(this.marka + ": zostal jedynie kolor " + this.color + " ");
+        console.log(this.marka + ": zostal jedynie kolor " + this.color);
+        console.log("1Y Warranty Cost: " + this.calculateWarrantyCost());
     };
     ;
+    Telefon.prototype.calculateWarrantyCost = function () {
+        this.warrantyCost = (this.cena * 1 / 12).toFixed(2);
+        return this.warrantyCost;
+    };
     return Telefon;
 }());
-var telefon1 = new Telefon('ipad', 6, 'niebieski');
-var telefon2 = new Telefon('samsung', 7, 'rozowy');
-telefon1.printInfo();
-telefon2.printInfo();
+var telefon1 = new Telefon('ipad', 20, 'niebieski').printInfo();
+var telefon2 = new Telefon('samsung', 50, 'rozowy').printInfo();
 //# sourceMappingURL=telefon.js.map
--- telefon.ts
+++ telefon.ts
@@ -1,16 +1,22 @@
 class Telefon {
 
-    constructor (private marka:string, private cena:number, private color:string){
+    warrantyCost:string;
+
+    constructor(private marka: string, private cena: number, private color: string) {
     }
 
-    printInfo(){
-        console.log(`${this.marka}: zostal jedynie kolor ${this.color} `);
+    printInfo() {
+        console.log(`${this.marka}: zostal jedynie kolor ${this.color}`);
+        console.log(`1Y Warranty Cost: ${this.calculateWarrantyCost()}`);
     };
+
+    private calculateWarrantyCost() {
+        this.warrantyCost = (this.cena * 1 / 12).toFixed(2);
+        return this.warrantyCost;
+    }
 }
 
+let telefon1 = new Telefon('ipad', 20, 'niebieski').printInfo();
+let telefon2 = new Telefon('samsung', 50, 'rozowy').printInfo();
 
-var telefon1 = new Telefon('ipad', 6, 'niebieski');
-var telefon2 = new Telefon('samsung',7, 'rozowy');
 
-telefon1.printInfo();
-telefon2.printInfo();
